ca3c391ae01963ea2313c2e673aa1e4d4bf9b227
Colin Nguyen <nguyen_c9@denison.edu>
2026-06-29 12:16:27 -0400

    Implement verify integrity function to check if two files are the same

--- venv/modules/hash.py
+++ venv/modules/hash.py
@@ -25,7 +25,19 @@ def hash_file(file_path):
     # return everything in hexadecimal    
     return h.hexdigest()
 
+
+def verify_integrity(file1, file2):
+    hash1 = hash_file(file1)
+    hash2 = hash_file(file2)
+    print("\nChecking integrity between ", file1, " and ", file2)
+
+    if hash1 == hash2:
+        return "File is intact, No modifications have been made."
+    
+    return "File has been modified. Possibly unsafe." 
+
 # this code will only run if we're running this 
 # specific file, acting as a tester
 if __name__ == "__main__":
     print("SHA Hash of File is: ", hash_file("C:/Users/colin/OneDrive/Desktop/Repos/cryptography-practice/venv/sample files/sample.txt"))
+    
